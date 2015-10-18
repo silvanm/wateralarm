9a4cdbf11c27b31c661a3318d2c1feb63d7d9ca7
silvan.muehlemann <silvan.muehlemann@muehlemann-popp.ch>
2015-10-18 18:36:57 +0200

    Sends Email to silvan@muehlemann.com in case of a status change.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,96 +11,111 @@ use AppBundle\Entity\Statusupdate;
 use Symfony\Component\Validator\Constraints\DateTime;
 
 
-class DefaultController extends Controller {
+class DefaultController extends Controller
+{
 
-	/**
-	 * @Route("/", name="statuspage")
-	 * @Template
-	 */
-	public function indexAction() {
-		return [ ];
-	}
+    /**
+     * @Route("/", name="statuspage")
+     * @Template
+     */
+    public function indexAction()
+    {
+        return [];
+    }
 
-	/**
-	 * Sends the statusupdates to the browser
-	 *
-	 * @Route("/get-status-json", name="getStatusJson")
-	 */
-	public function statusJsonAction() {
+    /**
+     * Sends the statusupdates to the browser
+     *
+     * @Route("/get-status-json", name="getStatusJson")
+     */
+    public function statusJsonAction()
+    {
 
-		/** @var Statusupdate $status */
-		$status = $this->getDoctrine()->getRepository( "AppBundle:Statusupdate" )->getStatus();
+        /** @var Statusupdate $status */
+        $status = $this->getDoctrine()->getRepository("AppBundle:Statusupdate")->getStatus();
 
-		/** @var Statusupdate $statuschange */
-		$statuschange = $this->getDoctrine()->getRepository( "AppBundle:Statusupdate" )->getLastStatuschange();
+        /** @var Statusupdate $statuschange */
+        $statuschange = $this->getDoctrine()->getRepository("AppBundle:Statusupdate")->getLastStatuschange();
 
 
-		switch ( $status->getWaterlevel() ) {
-			case 0:
-				$text         = 'OK.';
-				$class        = 'ok';
-				$buzzerstatus = 'silent';
-				break;
-			case 1:
-			case 2:
-				$text         = 'Warning';
-				$class        = 'warning';
-				$buzzerstatus = $status->getAlarmMuted() ? 'muted' : 'signaling';
-				break;
-			case 3:
-				$text         = 'ALARM!';
-				$class        = 'alarm';
-				$buzzerstatus = $status->getAlarmMuted() ? 'muted' : 'signaling';
-		}
+        switch ($status->getWaterlevel()) {
+            case 0:
+                $text         = 'OK.';
+                $class        = 'ok';
+                $buzzerstatus = 'silent';
+                break;
+            case 1:
+            case 2:
+                $text         = 'Warning';
+                $class        = 'warning';
+                $buzzerstatus = $status->getAlarmMuted() ? 'muted' : 'signaling';
+                break;
+            case 3:
+                $text         = 'ALARM!';
+                $class        = 'alarm';
+                $buzzerstatus = $status->getAlarmMuted() ? 'muted' : 'signaling';
+        }
 
-		if ($status->getCreatedAt()->diff(new \DateTime())->h > 0) {
-			$text = '???';
-			$class = '???';
-			$buzzerstatus = $status->getAlarmMuted() ? 'muted' : 'signaling';
-		}
+        if ($status->getCreatedAt()->diff(new \DateTime())->h > 0) {
+            $text         = '???';
+            $class        = '???';
+            $buzzerstatus = $status->getAlarmMuted() ? 'muted' : 'signaling';
+        }
 
-		$response = new Response( json_encode(
-			[
-				'text'         => $text,
-				'class'        => $class,
-				'lastUpdate'   => $status->getCreatedAt(),
-				'lastChange'   => $statuschange->getCreatedAt(),
-				'buzzerStatus' => $buzzerstatus,
-			]
+        $response = new Response(
+            json_encode(
+                [
+                    'text'         => $text,
+                    'class'        => $class,
+                    'lastUpdate'   => $status->getCreatedAt(),
+                    'lastChange'   => $statuschange->getCreatedAt(),
+                    'buzzerStatus' => $buzzerstatus,
+                ]
 
-		) );
-		$response->headers->set( 'Content-Type', 'application/json' );
+            )
+        );
+        $response->headers->set('Content-Type', 'application/json');
 
-		return $response;
-	}
+        return $response;
+    }
 
-	/**
-	 * Receives the status update from the arduino
-	 *
-	 * Protocol:
-	 * waterlevel = 0-3 (water level)
-	 * muted = 0/1 (whether the alarm is muted by a user
-	 *
-	 * @Route("/post-status", name="poststatus")
-	 */
-	public function statusAction( Request $request ) {
+    /**
+     * Receives the status update from the arduino
+     *
+     * Protocol:
+     * waterlevel = 0-3 (water level)
+     * muted = 0/1 (whether the alarm is muted by a user
+     *
+     * @Route("/post-status", name="poststatus")
+     */
+    public function statusAction(Request $request)
+    {
 
-		// check if it's a status change
-		/** @var Statusupdate $status */
-		$status = $this->getDoctrine()->getRepository( "AppBundle:Statusupdate" )->getStatus();
+        // check if it's a status change
+        /** @var Statusupdate $status */
+        $status = $this->getDoctrine()->getRepository("AppBundle:Statusupdate")->getStatus();
 
-		$waterlevel = $request->query->get( 'waterlevel' );
+        $waterlevel = $request->query->get('waterlevel');
 
-		$statusupdate = new Statusupdate();
-		$statusupdate->setCreatedAt( new \DateTime() );
-		$statusupdate->setWaterlevel( $waterlevel );
-		$statusupdate->setAlarmMuted( $request->query->get( 'muted' ) == 1 );
-		$statusupdate->setIsStatuschange( $status->getWaterlevel() != $waterlevel );
+        $statusupdate = new Statusupdate();
+        $statusupdate->setCreatedAt(new \DateTime());
+        $statusupdate->setWaterlevel($waterlevel);
+        $statusupdate->setAlarmMuted($request->query->get('muted') == 1);
+        if ($status->getWaterlevel() != $waterlevel) {
+            $statusupdate->setIsStatuschange(true);
+            mail(
+                "silvan@muehlemann.com",
+                "Wateralarm Status Change: ".$waterlevel,
+                "Waterlevel: $waterlevel\r\nMuted: ".$request->query->get('muted')
+            );
+        } else {
+            $statusupdate->setIsStatuschange(false);
+        }
 
-		$orm = $this->getDoctrine()->getManager();
-		$orm->persist( $statusupdate );
-		$orm->flush();
+        $orm = $this->getDoctrine()->getManager();
+        $orm->persist($statusupdate);
+        $orm->flush();
 
-		return new Response( "Status received" );
-	}
+        return new Response("Status received");
+    }
 }
